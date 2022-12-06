d891ec718bd22b44e20f0e4121e24f0b2def0631
Cody Garvin <emig647@gmail.com>
2022-12-06 08:46:07 -0800

    Added documentation and cleaned up code

--- app/src/main/java/com/codygarvin/platformscience/ShippingManager.kt
+++ app/src/main/java/com/codygarvin/platformscience/ShippingManager.kt
@@ -30,34 +30,20 @@ class ShippingManager {
         ShippingAddress("8725 Aufderhar River Suite 859"),
         ShippingAddress("79035 Shanna Light Apt. 322"))
 
+    /**
+     * Parse a number of drivers and addresses in order to find a match between the two.
+     * Note: This method is a brute force approach. It might be possible to use different data structures
+     * such as a max heap or weighted graph in order to optimize the method. This gets us up and running
+     * in an effort to explore the valid `secret` algorithm. These data structures may optimize for speed
+     * but may reduce readability.
+     */
     fun buildDriverIndex() {
-        var tempAddresses = shippingAddresses
-        for (addressIndex in shippingAddresses.indices) {
-            val address = shippingAddresses[addressIndex]
-
-            // build the scores for all the drivers for this address
-            var scores = mutableMapOf<Double, MutableList<Driver>>()
-
-            print("\n-----------\nBuilding scores for: ${address.address}")
-            for (driver in availableDrivers) {
-                val score = getScoreForAddressAndDriver(address, driver)
-                val driverList = scores[score]
-                if (driverList != null) {
-                    driverList.add(driver)
-                    scores[score] = driverList
-                } else {
-                    scores[score] = mutableListOf(driver)
-                }
-                print("\n$score - ${driver.driverName}")
-            }
-
-            // Associate scores with address
-            tempAddresses[addressIndex].driverScores = scores
-        }
+        // First calculate the scores for the drivers for each address
+        calculateScoresForDrivers()
 
         // Cycle through each address, removing each driver
         var tempRoutes = mutableListOf<Route>()
-        for (address in tempAddresses) {
+        for (address in shippingAddresses) {
             // Get driver with highest score, if driver is available. If no driver is found, try the next address
             val returnedDriver = getDriverForAddress(address, availableDrivers) ?: continue
 
@@ -71,6 +57,35 @@ class ShippingManager {
         // Only fill out the routes if it isn't empty
         filledAddresses = tempRoutes.ifEmpty {
             null
+        }
+    }
+
+    /**
+     * Cycle through each shipping address and calculate the score for each driver for that particular
+     * address.
+     */
+    private fun calculateScoresForDrivers() {
+        // Make a copy of the addresses so we can manipulate outside the array
+        for (address in shippingAddresses) {
+
+            // Start with empty scores so we can build them up for each driver. We are using a
+            // List for the drivers as drivers can have the same score.
+            var scores = mutableMapOf<Double, MutableList<Driver>>()
+
+            // Cycle through the available drivers and calculate their score for the address
+            for (driver in availableDrivers) {
+                val score = getScoreForAddressAndDriver(address, driver)
+                val driverList = scores[score]
+                if (driverList != null) {
+                    driverList.add(driver)
+                    scores[score] = driverList
+                } else {
+                    scores[score] = mutableListOf(driver)
+                }
+            }
+
+            // Associate scores with address
+            address.driverScores = scores
         }
     }
 
@@ -99,6 +114,12 @@ class ShippingManager {
         return returnDriver
     }
 
+    /**
+     * Calculate the score for the driver.
+     * If the street is even, use the vowels in a driver's name multiply by 1.5
+     * If the street is odd, use the consonants in the driver's name and multiply by 1
+     * If the street has a factor other than 1, multiply by 50%
+     */
     private fun getScoreForAddressAndDriver(address: ShippingAddress, driver: Driver): Double {
         var score = 0.0
         val driverAttributes = driver.driverAttributes()
@@ -116,6 +137,9 @@ class ShippingManager {
         return score
     }
 
+    /**
+     * Find the greatest common denominator to determine the highest factor
+     */
     private fun greatestCommonDenominator(driverLength: Int, streetLength: Int): Int {
         if (streetLength < 1) {
             return driverLength
--- app/src/test/java/com/codygarvin/platformscience/ShippingManagerTest.kt
+++ app/src/test/java/com/codygarvin/platformscience/ShippingManagerTest.kt
@@ -1,10 +1,22 @@
 package com.codygarvin.platformscience
 
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class ShippingManagerTest {
+    @Before
+    fun setup() {
+
+    }
+
+    @After
+    fun teardown() {
+        // May not be needed
+    }
+
     @Test
     fun `test basic calculation`() {
         // Arrange
@@ -14,6 +26,20 @@ class ShippingManagerTest {
         shippingManager.buildDriverIndex()
 
         // Assert
-        assert((shippingManager.filledAddresses?.count() ?: 0) > 1)
+        assert((shippingManager.filledAddresses?.count() ?: 0) == 10)
+    }
+
+    @Test
+    fun `test match driver`() {
+        // Arrange
+        val shippingManager = ShippingManager()
+
+        // Act
+        shippingManager.buildDriverIndex()
+
+        // Assert
+        assert((shippingManager.filledAddresses?.count() ?: 0) >= 3)
+        assert(shippingManager.filledAddresses?.get(2)?.driver?.driverName == "Noemie Murphy")
+        assert(shippingManager.filledAddresses?.get(2)?.address?.address == "7127 Kathlyn Ferry")
     }
 }
